cb8a5e617e71e2160acadd6a6506fd8469d75bc9
Bukva Miklós <miklos.bukva@bankmonitor.hu>
2019-10-21 20:13:14 +0200

    add js event handler

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 /*
  *  global site scripts
  */
+window.addEventListener('DOMContentLoaded', function (e) {
+    document.querySelector('hp-presentation').onclick = handlePresentationClick;
+});
+
+function handlePresentationClick(e) {
+    alert ('clicked')
+}
